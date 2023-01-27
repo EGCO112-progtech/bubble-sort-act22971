1cdeaa79c63fa4bc88d9471025922009ed612f03
Wichpol Vitayakomol <123149025+act22971@users.noreply.github.com>
2023-01-27 10:18:31 +0000

    test02

--- .ccls-cache/@home@runner@bubble-sort-act22971/main.c
+++ .ccls-cache/@home@runner@bubble-sort-act22971/main.c
@@ -9,7 +9,6 @@ N=argc-1;
 a=(int*)malloc(sizeof(int)*N);
 int *prime=(int*)malloc(sizeof(int)*N);
 for(i=0;i<N;i++) {
-
 a[i]=atoi(argv[i+1]);
   if(Isprime(a[i])){
     prime[j] = a[i];
@@ -19,13 +18,12 @@ a[i]=atoi(argv[i+1]);
   N = j;
     if(N==0){
   printf("No prime\n");}else{
-
    display(prime,N);
   bubbleSort(prime,N); 
    //insertion(a,N);
 
   // selectionSort(a,N);
-  // display(a,N);
+   display(a,N);
       }
  return 0;
 }
